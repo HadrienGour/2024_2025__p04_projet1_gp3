ca4490a435ac48916834d107ceb824548c4aa42e
HadrienGour <hadrien.gournay@orange.fr>
2024-09-30 08:45:49 +0200

    Update data.py

--- data.py
+++ data.py
@@ -1,1 +1,3 @@
-
+ask_for_the_init_number = int(input("entrez le nombre initial :"))
+ask_for_the_init_base = int(input("entrez la base initiale (2, 10, 16) :"))
+ask_for_the_target_base = int(input("entrez la base finale (2, 10, 16) :"))
