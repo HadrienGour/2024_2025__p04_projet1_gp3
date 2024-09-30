c5c7d194122991a09d23607271e454d69c246d6d
HadrienGour <hadrien.gournay@orange.fr>
2024-09-30 09:49:17 +0200

    Update data.py

--- data.py
+++ data.py
@@ -1,3 +1,7 @@
 ask_for_the_init_number = str(input("entrez le nombre initial :"))
 ask_for_the_init_base = int(input("entrez la base initiale (2, 10, 16) :"))
 ask_for_the_target_base = int(input("entrez la base finale (2, 10, 16) :"))
+
+bin_valid_chars = ["0", "1"]
+dec_valid_chars = bin_valid_chars + ["2", "3", "4", "5", "6", "7", "8", "9"]
+hex_valid_chars = dec_valid_chars + ["A", "B", "C", "D", "E", "F", "a", "b", "c", "d", "e", "f"]
