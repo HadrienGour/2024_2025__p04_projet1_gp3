059918043d54a8dd02a31c30e655973f6a3230f7
HadrienGour <hadrien.gournay@orange.fr>
2024-10-18 13:03:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -28,6 +28,10 @@ def do_the_job (init_number, init_base, target_base):
         elif is_in_selected_base (init_number, init_base) == "not in selected base":
            print ( "Le nombre n'existe pas dans cette base" )
         quit_or_continue = str(input("souhaitez vous continuer ? : "))
+        if quit_or_continue not in quit_responses:
+            init_number = str(input("entrez le nombre initial :"))
+            init_base = int(input("entrez la base de ce nombre (2, 10, 16) :"))
+            target_base = int(input("entrez la base dans laquelle vous souhaitez passer ce nombre (2, 10, 16) :"))
     return "end of program"
 
 do_the_job (ask_for_the_init_number, ask_for_the_init_base, ask_for_the_target_base)
