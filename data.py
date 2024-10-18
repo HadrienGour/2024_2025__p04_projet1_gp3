7b09dc99210106a62e41d3757de16fa0672a9f58
RaphOKB <kaitobzh@gmail.com>
2024-10-18 12:39:08 +0200

    mini update data
    déso g fait une faute d'inattention

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 ask_for_the_init_number = str(input("entrez le nombre initial :"))
 ask_for_the_init_base = int(input("entrez la base de ce nombre (2, 10, 16) :"))
-ask_for_the_target_base = int(input("entrez la base dans laquelle vous souhaitez passer ce nombre(2, 10, 16) :"))
+ask_for_the_target_base = int(input("entrez la base dans laquelle vous souhaitez passer ce nombre (2, 10, 16) :"))
 
 bin_valid_chars = ["0", "1"]
 dec_valid_chars = bin_valid_chars + ["2", "3", "4", "5", "6", "7", "8", "9"]
