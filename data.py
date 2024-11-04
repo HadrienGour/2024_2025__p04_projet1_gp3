2796ab99eaaad64bc36a57b0b514f9479585b484
RaphOKB <kaitobzh@gmail.com>
2024-11-04 08:22:18 +0100

    kaizen

--- data.py
+++ data.py
@@ -6,4 +6,4 @@ bin_valid_chars = ["0", "1"]
 dec_valid_chars = bin_valid_chars + ["2", "3", "4", "5", "6", "7", "8", "9"]
 hex_valid_chars = dec_valid_chars + ["A", "B", "C", "D", "E", "F"]
 
-quit_responses = ["non", "Non", "NON", "nan", "Nan", "NAN", "NON!", "No", "NO", "no"]
+quit_responses = ["non", "Non", "NON", "nan", "Nan", "NAN", "NON!", "No", "NO", "no", "quit", "Quit", "QUIT", "QUIT!", "exit", "Exit", "EXIT", "EXIT!", "sortir", "Sortir", "SORTIR", "sort", "Sort", "SORT"]
